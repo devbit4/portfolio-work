ea1b02dc1bfac60c2d88742b752d5af2e0074550
devbit4 <devbit4@gmail.com>
2022-01-13 20:22:30 +0900

    media query process part

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = (props) => {
+
+    const gnb = useRef(null);
     const active = {
         color: "#324B4C", fontWeight: "bold"
     }
+
+    const onToggle = (e) => {
+        gnb.current.classList.toggle("on");
+    }
     return (
         <header>
-            <div className="inner">
-                <h1><NavLink exact to="/">STORIES</NavLink></h1>
-                <ul id="gnb">
-                    <li><NavLink exact activeStyle={active} to="/about">ABOUT</NavLink></li>
-                    <li><NavLink exact activeStyle={active} to="/youtube">YOUTUBE</NavLink></li>
-                    <li><NavLink exact activeStyle={active} to="/gallery">GALLERY</NavLink></li>
-                    <li><NavLink exact activeStyle={active} to="/community">COMMUNITY</NavLink></li>
-                    <li><NavLink exact activeStyle={active} to="/contact">CONTACT</NavLink></li>
-                    <li><NavLink exact activeStyle={active} to="/join"><i className="fas fa-sign-in-alt"></i></NavLink></li>
-                </ul>
-            </div>
+            {/* <div className="inner"> */}
+            <h1><NavLink exact to="/">STORIES</NavLink></h1>
+            <ul id="gnb" ref={gnb}>
+                <li><NavLink exact activeStyle={active} to="/about">ABOUT</NavLink></li>
+                <li><NavLink exact activeStyle={active} to="/youtube">YOUTUBE</NavLink></li>
+                <li><NavLink exact activeStyle={active} to="/gallery">GALLERY</NavLink></li>
+                <li><NavLink exact activeStyle={active} to="/community">COMMUNITY</NavLink></li>
+                <li><NavLink exact activeStyle={active} to="/contact">CONTACT</NavLink></li>
+                <li><NavLink exact activeStyle={active} to="/join"><i className="fas fa-sign-in-alt"></i></NavLink></li>
+            </ul>
+
+            <button onClick={onToggle} className="toggle"><i className="fas fa-bars"></i></button>
+            {/* </div> */}
         </header>
     )
 }
