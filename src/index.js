48222dc4b79cbaedb4923da98599371eb68a4d80
devbit4 <devbit4@gmail.com>
2022-01-25 03:26:53 +0900

    redux-persist

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 import { Provider } from "react-redux";
-import store from "./redux/store";
 import AuthService from './service/auth_service';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { Persistor } from './redux/store';
 
 const authService = new AuthService();
 
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Provider store={store}>
-        <App authService={authService} />
+        <PersistGate loading={null} persistor={Persistor}>
+          <App authService={authService} />
+        </PersistGate>
       </Provider>
     </HashRouter>
   </React.StrictMode>,
