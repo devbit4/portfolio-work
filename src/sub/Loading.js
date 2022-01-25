223f1fe6f57f426552257824babf9dce0a0a710b
devbit4 <devbit4@gmail.com>
2022-01-25 20:31:11 +0900

    add loading bar

--- src/sub/Loading.js
+++ src/sub/Loading.js
@@ -4,6 +4,7 @@ const LoadingScreen = (props) => {
     return (
         <>
             <div className="loadingScreen">
+                <h1>LOADING...</h1>
                 <img
                     src={`${process.env.PUBLIC_URL}/img/start_loading.gif`}
                     alt="start_loading"
