877aa2f9600db069ea31060737845092df664cc8
devbit4 <devbit4@gmail.com>
2022-01-30 22:26:12 +0900

    test

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -2,9 +2,9 @@ import firebase from 'firebase';
 // import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyELXXMDfjz2cR2yN3_Q32sQOuj54KW5Y",
-    authDomain: "portfolio-6327e.firebaseapp.com",
-    projectId: "portfolio-6327e"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
     // appId: "1:927965447454:web:d41c316c5fc303ca21750f",
 
 };
